f5ee4172828811484cd17aa93fbdb4e39efdc8e6
jose <josechagas31@gmail.com>
2023-05-26 16:12:58 -0300

    Alinhando a página

---
+++ script.js
@@ -0,0 +1,14 @@
+let n1 = prompt("digite um número: ");
+
+let n2 = prompt("Digite outro número: ");
+
+
+let soma = Number(n1) + Number(n2) ;
+
+
+
+if ( soma > 60 ) {
+  console.log("Aprovado " + soma);
+} else {
+  console.log("Reprovado " + soma);
+}
